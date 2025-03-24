3fd02b434840fcc277ba50b8434a924c31ce739b
Sarif167 <wajsarif461@gmail.com>
2025-03-24 16:00:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,10 +14,10 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+ADMIN = int(getenv("ADMIN", "1249672673"))
+API_ID = int(getenv("API_ID", "26597768"))
+API_HASH = str(getenv("API_HASH", "340f87444e648a712eae77c310648e21"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "8023193614:AAFzQOvIth3DUb1NWaK0_qZmdRHmwUSv5ac"))
 MONGO_DB = str(
     getenv(
         "MONGO_DB",
